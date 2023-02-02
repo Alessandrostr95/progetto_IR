4e67b8753d337633bb0162c593989cca5cb06f24
Alessandrostr95 <alessandrostr95@gmail.com>
2023-02-02 15:25:08 +0100

    [solr] cartella rinominata solr-8.11.2 -> solr

--- src/main/java/it/uniroma2/informatica/magistrale/ir/imdbseries/ImdbSeriesApplication.java
+++ src/main/java/it/uniroma2/informatica/magistrale/ir/imdbseries/ImdbSeriesApplication.java
@@ -1,5 +1,7 @@
 package it.uniroma2.informatica.magistrale.ir.imdbseries;
 
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @SpringBootApplication
 @RestController
 public class ImdbSeriesApplication {
+
+	static String url = "http://localhost:8983/solr";
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbSeriesApplication.class, args);
@@ -23,4 +27,13 @@ public class ImdbSeriesApplication {
 	public String home() {
 		return "<h1>Home Page</h1>";
 	}
+
+	private SolrClient getClient() {
+		return new HttpSolrClient.Builder(url)
+				.withConnectionTimeout(10000)
+				.withSocketTimeout(60000)
+				.build();
+	}
+
+
 }
