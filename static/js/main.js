c6a9ccd11c3a606308ec2ba6031604879b3a8396
Michele <michelecirillo1999@live.it>
2023-02-14 17:27:12 +0100

    Front parziale

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,36 @@ window.addEventListener("load", function () {
     const search_form = document.getElementById("search_form");
     search_form.onsubmit = (e) => {
         const fields = document.querySelectorAll(".read-value");
-        const j = {};
-        /*fields.forEach(field =>
-            console.log(field.name + " " + getValue(field))
-        );*/
-        fields.forEach(field => j[field.name] = getValue(field));
-        console.log(j);
-        // const select = fields[2];
-        // console.log(getSelectValues(select));
+        const json = { fields: {} };
+        jsonFields = json["fields"];
+        // Fields with read-value class
+        fields.forEach(field => jsonFields[field.name] = getValue(field));
+        // Fields with Genre class
+        jsonFields["Genre"] = {
+            "op": document.querySelector('input[class="Genre"]:checked').value,
+            "values": getSelectValues(document.querySelector('select.Genre'))
+        }
+        jsonFields["Actors"] = {
+            "op": document.querySelector('input[class="Genre"]:checked').value,
+            "values": getSelectValues(document.querySelector('select.Actors'))
+        }
+        jsonFields["boost"] = {};
+        this.document.querySelectorAll("input.boost").forEach(
+            boost => {
+                jsonFields["boost"][boost.name] = getValue(boost);
+            }
+        );
+        console.log(json);
         return false;
+    }
+
+    // Show/hide advanced params onclick function
+    this.document.getElementById("advanced_params").onclick = () => {
+        const paramsDiv = document.querySelector("#advanced_params + div");
+        paramsDiv.classList.toggle("hide");
+        const icon = document.querySelector("#advanced_params i");
+        icon.classList.toggle("fa-arrow-up");
+        icon.classList.toggle("fa-arrow-down");
     }
 });
 
